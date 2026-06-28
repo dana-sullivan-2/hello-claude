4e7e51f5138f2f222423dfabf51c6dc3ccfd560a
Dana Sullivan <dana.sullivan.contact@proton.me>
2026-06-28 19:26:29 -0400

    Remove dynamic random_data table script

--- create_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE random_data (
-    id SERIAL PRIMARY KEY,
-    random_number INT NOT NULL
-);
-
-INSERT INTO random_data (random_number)
-SELECT FLOOR(RANDOM() * 100 + 1)::INT
-FROM generate_series(1, 20);
